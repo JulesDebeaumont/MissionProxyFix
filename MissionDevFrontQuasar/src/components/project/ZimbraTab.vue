a738caa99459a6580a54e7a5694cdf9bc0e22af5
Jules Debeaumont <deb.jules@outlook.fr>
2024-06-08 01:05:52 +0200

    Service rewrite WIP

--- MissionDevFrontQuasar/src/components/project/ZimbraTab.vue
+++ MissionDevFrontQuasar/src/components/project/ZimbraTab.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { api } from 'src/boot/axios';
 import { useUserStore } from 'src/stores/user-store';
-import { computed, ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import { IZimbraMailPreview, IZimbraMail } from 'src/components/models';
 import { dateFormatDDMMYYYYHHMM } from 'src/utils/datetime';
 
@@ -14,8 +14,13 @@ const propsComponent = defineProps<{
 const userStore = useUserStore();
 
 // refs
+const isLoadingGetFolder = ref(false);
 const isLoadingAuthRef = ref(false);
 const isLoadingGetMailsRef = ref(false);
+const isLoadingGetMailRef = ref(false);
+const setupFolderMode = ref(false);
+const connexionMode = ref(false);
+const currentUserFolderMail = ref<string | null>(null);
 const mailsPreviewRef = ref<IZimbraMailPreview[] | null>(null);
 const mailRef = ref<IZimbraMail | null>(null);
 const passwordInputRef = ref('');
@@ -28,19 +33,21 @@ async function getZimbraAuthToken() {
       password: passwordInputRef.value,
     });
     userStore.setupZimbraAuthToken(responseZimbraCrsf.data as string);
+    connexionMode.value = false;
+    await getUserFolderMailForProject();
   } catch (error) {
     console.log(error);
   } finally {
     isLoadingAuthRef.value = false;
   }
 }
-async function getZimbraMailByFolder(folderName: string) {
+async function getZimbraMail() {
   isLoadingGetMailsRef.value = true;
   try {
-    const responseZimbraEmailPreview = await api.post(`GetMailsByFolder`, {
-      FolderName: folderName,
-      MailCount: 100,
-      AuthToken: userStore.getZimbraAuthTokenInCookie,
+    const responseZimbraEmailPreview = await api.post(`MailBox/GetMails`, {
+      projectId: propsComponent.projectId,
+      mailCount: 100,
+      authToken: userStore.getZimbraAuthTokenInCookie,
     });
     mailsPreviewRef.value =
       responseZimbraEmailPreview.data as IZimbraMailPreview[];
@@ -51,14 +58,49 @@ async function getZimbraMailByFolder(folderName: string) {
   }
 }
 async function getZimbraMailById(mailId: string) {
+  isLoadingGetMailRef.value = true;
   try {
-    const responseZimbraEmailPreview = await api.post(`GetMailById`, {
+    const responseZimbraEmailPreview = await api.post(`MailBox/GetMailById`, {
       mailId,
       AuthToken: userStore.getZimbraAuthTokenInCookie,
     });
     mailRef.value = responseZimbraEmailPreview.data as IZimbraMail;
   } catch (error) {
     console.log(error);
+  } finally {
+    isLoadingGetMailRef.value = false;
+  }
+}
+async function getUserFolderMailForProject() {
+  isLoadingGetFolder.value = true;
+  try {
+    const responseUserFolder = await api.get(
+      `Projects/${propsComponent.projectId}/GetFolderName`
+    );
+    currentUserFolderMail.value = responseUserFolder.data.mailFolderName;
+    if (currentUserFolderMail.value === null) {
+      setupFolderMode.value = true;
+    } else {
+      await getZimbraMail();
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoadingGetFolder.value = false;
+  }
+}
+async function setUserFolderMailForProject() {
+  isLoadingGetFolder.value = true;
+  try {
+    await api.put(`Projects/${propsComponent.projectId}/SetFolderName`, {
+      folderName: currentUserFolderMail.value,
+    });
+    setupFolderMode.value = false;
+    await getZimbraMail();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoadingGetFolder.value = false;
   }
 }
 function formatMailHtmlForImages(mailHtmlContent: string, mail: IZimbraMail) {
@@ -70,7 +112,7 @@ function formatMailHtmlForImages(mailHtmlContent: string, mail: IZimbraMail) {
         const part = findPartInMpObject(partIdentifier, mail.mp);
         mailHtmlContent = mailHtmlContent.replace(
           occurence[0],
-          `src="${process.env.API_URL}Projects/GetZimbraMailAttachment?MailId=${mail.id}&Part=${part}&AuthToken=${userStore.getZimbraAuthTokenInCookie}"`
+          `src="${process.env.API_URL}MailBox/GetAttachment?MailId=${mail.id}&Part=${part}&AuthToken=${userStore.getZimbraAuthTokenInCookie}&ProjectId=${propsComponent.projectId}&Filename=image.png"`
         );
       }
     }
@@ -136,18 +178,64 @@ const htmlMail = computed(() => {
   }
   return '';
 });
+
+// lifeCycle
+onMounted(async () => {
+  if (userStore.zimbraAuthTokenInCookie === false) {
+    connexionMode.value = true;
+  } else {
+    await getZimbraMail();
+  }
+});
 </script>
 
 <template>
-  <q-input label="Password" v-model="passwordInputRef" />
+  <div v-if="connexionMode">
+    Veuillez saisir votre mot de passe Zimbra (promis je le regarde pas).
+    <q-form @submit="getZimbraAuthToken">
+      <q-input
+        label="Mot de passe"
+        type="password"
+        v-model="passwordInputRef"
+      />
+      <q-btn
+        type="submit"
+        label="Connexion"
+        no-caps
+        color="primary"
+        class="q-mt-sm"
+      />
+    </q-form>
+  </div>
 
-  <q-btn @click="getZimbraAuthToken" label="Zimbra Auth !" color="primary" />
-  <q-btn
-    @click="getZimbraMailByFolder('homepop')"
-    label="Get Mails"
-    color="primary"
-  />
-  <template v-if="isLoadingGetMailsRef === false">
+  <div v-if="setupFolderMode">
+    <div>
+      Veuillez saisir le nom du dossier Zimbra auquel vous voulez associez le
+      projet.
+    </div>
+    <q-form @submit="setUserFolderMailForProject">
+      <q-input
+        v-model="currentUserFolderMail"
+        label="Nom du dossier Zimbra"
+        filled
+      />
+      <q-btn
+        type="submit"
+        label="Enregistrer"
+        no-caps
+        color="primary"
+        class="q-mt-sm"
+      />
+    </q-form>
+  </div>
+
+  <template
+    v-if="
+      isLoadingGetMailsRef === false &&
+      setupFolderMode === false &&
+      connexionMode === false
+    "
+  >
     <div class="flex row">
       <q-list
         v-if="mailsPreviewRef !== null"
@@ -162,10 +250,10 @@ const htmlMail = computed(() => {
           class="rounded-borders"
           :class="
             mailPreview.f === 'u'
-              ? 'bg-light-blue-2'
+              ? 'bg-grey-4'
               : mailPreview.id === mailRef?.id
-              ? 'bg-primary'
-              : 'bg-white'
+              ? 'bg-green-2'
+              : ''
           "
           @click="getZimbraMailById(mailPreview.id)"
         >
@@ -182,15 +270,22 @@ const htmlMail = computed(() => {
         </q-item>
       </q-list>
       <div
-        v-if="mailRef !== null && htmlMail !== null"
-        class="col-6"
+        v-if="
+          mailRef !== null && htmlMail !== null && isLoadingGetMailRef === false
+        "
+        class="col-6 q-pa-md"
         v-html="formatMailHtmlForImages(htmlMail, mailRef)"
       ></div>
-      {{ htmlMail ?? 'sdfsdf' }}
+      <div v-show="isLoadingGetMailRef" class="flex">
+        <q-spinner color="primary" size="md" />
+      </div>
     </div>
   </template>
 
-  <div v-show="isLoadingGetMailsRef" class="flex flex-center">
+  <div
+    v-show="isLoadingGetMailsRef || isLoadingGetFolder"
+    class="flex flex-center"
+  >
     <q-spinner color="primary" size="md" />
   </div>
 </template>
