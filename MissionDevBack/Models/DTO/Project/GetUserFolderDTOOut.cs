f020041279491651c2240a67878bea6dc74b5e1b
Jules Debeaumont <deb.jules@outlook.fr>
2024-10-23 17:20:32 +0200

    [ADD] shader for grass

---
+++ MissionDevBack/Models/DTO/Project/GetUserFolderDTOOut.cs
@@ -0,0 +1,7 @@
+namespace MissionDevBack.Models;
+
+public class GetUserFolderDTOOut
+{
+    public string UserId { get; set; }
+    public string MailFolderName { get; set; }
+}
---
+++ MissionDevBack/Models/DTO/Project/SetFolderNameDTOIn.cs
@@ -0,0 +1,6 @@
+namespace MissionDevBack.Models;
+
+public class SetFolderNameDTOIn
+{
+    public string FolderName { get; set; }
+}
